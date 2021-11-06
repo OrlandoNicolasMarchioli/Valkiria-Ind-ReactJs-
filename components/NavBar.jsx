a906984d217732a640f15b79622019378d09877f
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-11-06 18:18:42 -0300

    primer commit

---
+++ components/NavBar.jsx
@@ -0,0 +1,12 @@
+
+
+function NavBar(){
+    return(
+        <nav>
+            <li>Hola</li>
+            <li>Soy</li>
+            <li>Una</li>
+            <li>NavBar</li>
+        </nav>
+    )
+}
