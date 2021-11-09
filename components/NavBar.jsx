8dd53692eb29efc93ec5d7bd5fb91aaf316afcd2
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-11-09 19:10:55 -0300

    Ingreso de carpetas

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,10 +3,24 @@
 function NavBar(){
     return(
         <nav>
-            <li>Hola</li>
-            <li>Soy</li>
-            <li>Una</li>
-            <li>NavBar</li>
+            <li><a>Inicio</a></li>
+            <li>
+                <a>Productos</a>
+                <ul>
+                    <li><a href="#">Abrigos</a></li>
+                    <li><a href="#">Pantalones</a></li>
+                    <li><a href="#">Remeras</a></li>
+                    <li><a href="#">Accesorios</a></li>
+                </ul>
+            </li>
+            <li><a>Contacto</a></li>
+            <li>
+                <a>Search</a>
+                <prompt></prompt>
+            </li>
         </nav>
     )
 }
+
+
+export default NavBar;
