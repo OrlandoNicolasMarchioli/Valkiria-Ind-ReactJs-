47deaabb5f534dccbf3877c2c7297e42f5535bbf
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-11-30 00:23:11 -0300

    entrega de enrutado

--- src/components/itemConstructor.jsx
+++ src/components/itemConstructor.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Item from './item'
 import Carrousel from './Carrousel'
-import ItemDetailContainer from './itemDetailContainer'
+/* import ItemDetailContainer from './itemDetailContainer' */
 const Constructor = () => {
     return (
         <section className="sectionConstructorItems">
             {Carrousel()}
             {Item()}
-            {ItemDetailContainer()}
         </section>
     )
 }
