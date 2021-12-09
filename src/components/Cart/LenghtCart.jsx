1c148fb0354e9f8936f1b0fee7e3430e33436a6a
Marchioli Orlando Nicolas <nico.marchioli@gmail.com>
2021-12-09 19:59:36 -0300

    arreglando el codicional de cart

---
+++ src/components/Cart/LenghtCart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Cart from './cart'
+import CartEmpty from './cartEmpty'
+
+const LenghtCart = (value) => {
+    if (value.length() == 0){
+        value = false
+    }else{
+        value = true
+    }
+    return (
+        value.condition ? <Cart/> : <CartEmpty/>
+    )
+}
+
+export default LenghtCart
--- src/components/Cart/cartContext.jsx
+++ src/components/Cart/cartContext.jsx
@@ -1,13 +1,22 @@
 import React, {createContext, useState} from "react";
+import LenghtCart from "./LenghtCart";
 
 export const CartContext= createContext()
 
 const CartProvider=(props)=>{
     const [cart,setCart] = useState([]);
+    const Length = (cart)=>{
+        if (cart.Length() == 0) {
+            return false
+        } else {
+            return true
+        }
+    }
 
     return(
         <CartContext.Provider value ={[cart,setCart]}>
             {props.children}
+            {cart.map((value)=>{return(LenghtCart(cart.value))})}
         </CartContext.Provider>
     )
 }
